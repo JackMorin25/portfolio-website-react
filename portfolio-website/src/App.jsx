d2757ca5fc3952d0ddd77d65fe03c6e3558c0eb0
JackMorin25 <morjm-25@rhodes.edu>
2025-07-29 10:42:09 -0500

    added some text which fades in on render and added linkedin logo

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,4 +1,5 @@
 import githubLogo from './assets/github-mark-white.svg'
+import linkedInLogo from './assets/linkedin-svgrepo-com.svg'
 import PixelRain from './PixelRain'
 import FadeText from './components/fade-text/fadeText'
 import './App.css'
@@ -12,6 +13,9 @@ function App() {
       <div>
         <a href="https://github.com/JackMorin25" target="_blank">
           <img src={githubLogo} className="logo" alt="GitHub logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/jack-morin-13a83b294" target="_blank">
+          <img src={linkedInLogo} className="logo" alt="LinkedIn logo" />
         </a>
       </div>
       <FadeText />  
